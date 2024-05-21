a9c60e0d600f4f17d53e40cb71c9d005142c9569
river <jenn0.6n@gmail.com>
2024-05-21 14:58:28 +0900

    deploy: 배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      web3: "web3/dist/web3.min.js",
+    },
+  },
   base: "/react-shopping-cart/dist",
 });
